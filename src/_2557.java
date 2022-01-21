050e8dec7611d3f66afc90b0087db8cf659af271
천지홍 <clara5091@naver.com>
2022-01-21 21:03:25 +0900

    Initial commit

---
+++ src/_2557.java
@@ -0,0 +1,8 @@
+public class _2557 {
+    public static void main(String[] args){
+        int x=2, y=10;
+        System.out.println("Hello World!");
+        int z = y%2;
+        System.out.println(z);
+    }
+}
