cec9879837b7638332c9b47c21459dd9871369e2
천지홍 <clara5091@naver.com>
2022-01-27 23:21:47 +0900

    22/01/27

---
+++ src/_1085.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class _1085 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x=sc.nextInt(), y= sc.nextInt(), w=sc.nextInt(), h=sc.nextInt(), min1, min2;
+        min1=((w-x)>x)?x:w-x;
+        min2=((h-y)>y)?y:h-y;
+        System.out.println(Math.min(min1, min2));
+    }
+}
