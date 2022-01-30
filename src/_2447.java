47c2fa474fdd37da149df23926d5ef5f98c3b1a4
천지홍 <clara5091@naver.com>
2022-01-30 23:06:46 +0900

    22/01/30

--- src/_2447.java
+++ src/_2447.java
@@ -3,6 +3,23 @@ import java.util.Scanner;
 public class _2447 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[][] ar = new String[n][n];
+        star(0, 0, n, ar, true);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++) System.out.print(ar[i][j]);
+            System.out.println("");
+        }
+        sc.close();
+    }
 
+    public static void star(int x, int y, int n, String[][] ar, boolean b){
+        if(n==1) {
+            if(b) ar[x][y] = "*";
+            else ar[x][y]=" ";
+        }
+        else{
+
+        }
     }
 }
