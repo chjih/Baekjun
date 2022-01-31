1edfc52a733f7415bae03bdcebeb57db09335522
천지홍 <clara5091@naver.com>
2022-01-31 17:44:03 +0900

    22/01/31

---
+++ src/_11729.java
@@ -0,0 +1,30 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
+public class _11729 {
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println((int)Math.pow(2, n)-1);
+        hinoy(n, 1, 2, 3);
+        bw.flush();
+        bw.close();
+        sc.close();
+    }
+    public static void hinoy(int n, int s, int m, int e) throws IOException {
+        if(n==1){
+            bw.write(s+" "+e+"\n");
+        }
+        else{
+            //n-1만큼 중간으로 옮김
+            hinoy(n-1, s, e, m);
+            //가장 큰 원판 맨 끝으로 옮김
+            bw.write(s+" "+e+"\n");
+            //중간의 그 n-1판을 가장 끝에있는 큰 원판에 올린다.
+            hinoy(n-1, m, s, e);
+        }
+    }
+}
--- src/_2447.java
+++ src/_2447.java
@@ -1,16 +1,19 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class _2447 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(bf.readLine());
         String[][] ar = new String[n][n];
         star(0, 0, n, ar, true);
         for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++) System.out.print(ar[i][j]);
-            System.out.println("");
+            for(int j=0; j<n; j++) bw.write(ar[i][j]);
+            bw.write("\n");
         }
-        sc.close();
+        bw.flush();
+        bw.close();
+        bf.close();
     }
 
     public static void star(int x, int y, int n, String[][] ar, boolean b){
@@ -19,7 +22,12 @@ public class _2447 {
             else ar[x][y]=" ";
         }
         else{
-
+            for(int i=0; i<3; i++){
+                for(int j=0; j<3; j++){
+                    if(i==1&&j==1)star(x+i*(n/3), y+j*(n/3), n/3, ar, false);
+                    else star(x+i*(n/3), y+j*(n/3), n/3, ar, b);
+                }
+            }
         }
     }
 }
