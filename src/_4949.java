c55cd26df53b81dc127bcc0832e07287966d7730
천지홍 <clara5091@naver.com>
2022-02-22 23:43:02 +0900

    22/02/22

---
+++ src/_4949.java
@@ -0,0 +1,33 @@
+import java.util.EmptyStackException;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class _4949 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            String s = sc.nextLine();
+            if(s.equals(".")) break;
+            int i;
+            Stack<Character> stack = new Stack<>();
+            try{
+                for(i=0; i<s.length(); i++){
+                    if(s.charAt(i)=='('||s.charAt(i)=='[') stack.add(s.charAt(i));
+                    else if(s.charAt(i)==')'){
+                        if(stack.peek()=='(') stack.pop();
+                        else break;
+                    }
+                    else if(s.charAt(i)==']'){
+                        if(stack.peek()=='[') stack.pop();
+                        else break;
+                    }
+                }
+                if(i==s.length()&&stack.size()==0) System.out.println("yes");
+                else System.out.println("no");
+            } catch(EmptyStackException e){
+                System.out.println("no");
+            }
+        }
+        sc.close();
+    }
+}
---
+++ src/_9012.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class _9012 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0; i<n; i++){
+            String s = sc.next();
+            Stack<Character> stack = new Stack<>();
+            int j;
+            for(j=0; j<s.length(); j++){
+                char c = s.charAt(j);
+                if(c=='('){
+                    stack.add(c);
+                }
+                else{
+                    if(stack.size()==0) break;
+                    stack.pop();
+                }
+            }
+            if(j==s.length()&&stack.size()==0) System.out.println("YES");
+            else System.out.println("NO");
+        }
+        sc.close();
+    }
+}
